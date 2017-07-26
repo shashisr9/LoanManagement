55eb850ddd3da0d6c30cdee992c70df3692907a4
shashisr9 <srshashidhar7@gmail.com>
2017-07-26 21:03:47 +0530

    Update User.java

--- src/main/java/com/hcl/loan/model/User.java
+++ src/main/java/com/hcl/loan/model/User.java
@@ -1,5 +1,151 @@
 package com.hcl.loan.model;
 
+import java.time.LocalDate;
+
 public class User {
 
+	private Long userId;
+	private String bankAccountNumber;
+	private String bankName;
+	private String ifscCode;
+	private String officeAddress;
+	private String repaymentMode;
+	private String preferredLanguage;
+	private String firstName;
+	private Address address;
+	private Long mobileNumber;
+	private String lastName;
+	private String password;
+	private String emailAddress;
+	private LocalDate birthDate;
+	private String gender;
+	private boolean hniFlag;
+
+	public boolean isHniFlag() {
+		return hniFlag;
+	}
+
+	public void setHniFlag(boolean hniFlag) {
+		this.hniFlag = hniFlag;
+	}
+
+	public void setMobileNumber(Long mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public long getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(long mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public String getBankAccountNumber() {
+		return bankAccountNumber;
+	}
+
+	public void setBankAccountNumber(String bankAccountNumber) {
+		this.bankAccountNumber = bankAccountNumber;
+	}
+
+	public String getBankName() {
+		return bankName;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setBankName(String bankName) {
+		this.bankName = bankName;
+	}
+
+	public String getIfscCode() {
+		return ifscCode;
+	}
+
+	public void setIfscCode(String ifscCode) {
+		this.ifscCode = ifscCode;
+	}
+
+	public String getOfficeAddress() {
+		return officeAddress;
+	}
+
+	public void setOfficeAddress(String officeAddress) {
+		this.officeAddress = officeAddress;
+	}
+
+	public String getRepaymentMode() {
+		return repaymentMode;
+	}
+
+	public void setRepaymentMode(String repaymentMode) {
+		this.repaymentMode = repaymentMode;
+	}
+
+	public String getPreferredLanguage() {
+		return preferredLanguage;
+	}
+
+	public void setPreferredLanguage(String preferredLanguage) {
+		this.preferredLanguage = preferredLanguage;
+	}
 }
